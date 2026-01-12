cdffeb73c6722d4dac17baa97bdfb1ecf09164e6
Minhlopici <mikehype44444@gmail.com>
2026-01-12 12:10:24 +0700

    Add files via upload

---
+++ Assignment1/Work3.py
@@ -0,0 +1,4 @@
+
+x = input("Please type the length of a rectangle:")
+y = input("Please type the width of a rectangle:")
+print("the perimeter and the area of the rectangle are:", (float(x) * 2 + float(y) * 2) ,"and" , float(1) * float(y))
