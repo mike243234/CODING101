f6a902151d003b73efa5af80f6ce4ac509f36cc9
Minhlopici <mikehype44444@gmail.com>
2026-01-12 12:00:41 +0700

    Add files via upload

---
+++ Work1.py
@@ -0,0 +1,2 @@
+x = input("Xin hãy nhập tên của bạn:")
+print("Xin chào", x )
---
+++ Work2.py
@@ -0,0 +1,4 @@
+import math
+
+x = input("Please type a radius of a circle:")
+print("the circumference of a circle is:", round(math.pi * float(x), 2))
