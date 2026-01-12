bc6f4881293abcda8a44997628f9cb035a3a8aae
Minhlopici <mikehype44444@gmail.com>
2026-01-12 12:11:50 +0700

    Delete Work4.py

--- Work4.py
+++
@@ -1,5 +0,0 @@
-
-x, y, z = map(int,input("please type 3 interger number:").strip().split())
-print('The sum of 3 integer number is:', x + y + z)
-print("The average of 3 integer number is:", (x + y + z) / 3)
-print('The product of 3 integer number is:', x * y * z)
